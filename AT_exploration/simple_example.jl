f93463c981b0123a9135e59b06ecda3b6450b932
jevdnlan <jeppe.vdl@gmail.com>
2024-10-16 22:04:02 +0200

    add simple_example.jl

---
+++ AT_exploration/simple_example.jl
@@ -0,0 +1,132 @@
+using Catalyst, OrdinaryDiffEq, Plots, Graphs, GraphRecipes, JumpProcesses, DataFrames
+#functions--------------------------------------------------------------------------------
+
+#function to create a reaction network
+function create_reactions(building_blocks, upper_bound, rate)
+    species = building_blocks
+    # create all possible species under upper bound by combining building blocks
+    for i in 2:upper_bound 
+        combinations = vec(map(collect, Iterators.product(ntuple(_ -> building_blocks, i)...)))
+        spec = [join(c) for c in combinations]
+        species = vcat(species, spec)
+    end
+
+    # create expressions
+    species = [sp * "(t)" for sp in species] .|> Meta.parse
+    species = [only(@eval @species $(element)) for element in species]
+
+    # create all possible reactions
+    reactions = []
+    for str1 in species
+        for str2 in species
+            len_comb = length(string(str1)*string(str2)) - 6
+            if len_comb <= upper_bound
+                comb = replace(string(str1), "(t)" => "") * string(str2) .|> Meta.parse
+                comb = only(@eval @species $(comb))
+                if string(str1) == string(str2) 
+                    push!(reactions, Reaction(rate, [str1], [comb], [2], [1]))
+                else
+                    push!(reactions, Reaction(rate, [str1, str2], [comb], [1, 1], [1]))
+                end
+            end
+        end
+    end
+    return reactions, species
+end
+
+# Get assembly index of target `t` (from assembly_indexV3.jl)
+function assembly_index(t; return_visited = false)
+    A, visited = _assembly_index(t, String[])
+    if return_visited
+        return A, visited
+    else
+        return A
+    end
+end
+
+# Function calculating the assembly index of a compound `s`
+function _assembly_index(s, visited) # aka "AssemblyIndex"
+    if s in visited
+        # no construction cost for previously visited compounds
+        return 0, visited 
+    elseif length(s) <= 3
+        # strings of length <= 3 can only be constructed in a set amount of combinations
+        return length(s) - 1, [visited; s]
+    else
+        # not yet visited "complex" compound => split into substructures
+        substructure_pairs = getsubstructures(s)
+
+        As = Vector{Int64}(undef, length(substructure_pairs))
+        visiteds = Vector{Vector{String}}(undef, length(substructure_pairs))
+
+        for (sp_idx, sp) in enumerate(substructure_pairs)
+            A1, visited1 = _assembly_index(sp[1], visited)
+            A2, visited2 = _assembly_index(sp[2], [visited1; sp[1]])
+            As[sp_idx] = A1 + A2 + 1
+            visiteds[sp_idx] = [visited2; sp[2]]
+        end
+
+        min_idx = argmin(As)
+        return As[min_idx], visiteds[min_idx]
+    end
+end
+
+function getsubstructures(s)
+    return [[s[1:i], s[i+1:end]] for i in 1:(length(s)-1)]
+end
+#-----------------------------------------------------------------------------------------
+# setting up model
+
+building_blocks = ["A", "B", "N"]
+upper_bound = 6
+rate = 1
+t = default_t()
+
+reactions, species = create_reactions(building_blocks, upper_bound, rate);
+@named jumpmodel = ReactionSystem(reactions, t)
+
+u0 = vcat([Symbol(replace(string(sp), "(t)" => "")) => 500 for sp in species[1:length(building_blocks)]], [Symbol(replace(string(sp), "(t)" => "")) => 0 for sp in species[length(building_blocks)+1:end]])
+tspan = (0.0, 1.0)
+ps = []
+
+prob = DiscreteProblem(complete(jumpmodel), u0, tspan, ps)
+
+jump_prob = JumpProblem(complete(jumpmodel), prob, Direct())
+
+sol = solve(jump_prob, SSAStepper())
+
+#-----------------------------------------------------------------------------------------
+#Plot by length
+species_symbols = [Symbol(replace(string(sp), "(t)" => "")) for sp in species]
+sol_lengths = map(length, map(string, species_symbols))
+df = DataFrame(name = species_symbols, length = sol_lengths)
+
+p = plot(title = "Sum of Variables by Length", xlabel = "Time", ylabel = "Sum");
+
+for len in unique(sol_lengths)
+
+    group_indices = findall(df.length .== len)
+
+    group_sum = [sum([sol.u[t][idx] for idx in group_indices]) for t in 1:length(sol.u)]
+
+    plot!(p, 1:length(sol.u), group_sum, label = "Length $len")
+end
+
+display(p)
+
+#Plot by assembly index
+sol_ai = map(assembly_index, map(string, species_symbols))
+df_ai = DataFrame(name = species_symbols, ai = sol_ai)
+
+p_ai = plot(title = "Sum of Variables by Assembly Index", xlabel = "Time", ylabel = "Sum");
+
+for ai in unique(sol_ai)
+
+    group_indices = findall(df_ai.ai .== ai)
+
+    group_sum = [sum([sol.u[t][idx] for idx in group_indices]) for t in 1:length(sol.u)]
+
+    plot!(p_ai, 1:length(sol.u), group_sum, label = "Assembly Index $ai")
+end
+
+display(p_ai)
