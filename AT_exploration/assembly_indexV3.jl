b7e24babedad2781a289136e0a2ddf7a42b2d399
jevdnlan <jeppe.vdl@gmail.com>
2024-10-22 10:27:44 +0200

    added dictionary to assembly index

--- AT_exploration/assembly_indexV3.jl
+++ AT_exploration/assembly_indexV3.jl
@@ -1,6 +1,7 @@
 # Get assembly index of target `t`
 function assembly_index(t; return_visited = false) # aka "Main"
-    A, visited = _assembly_index(t, String[])
+    substruct_dic = Dict{String, Int64}()
+    A, visited = _assembly_index(t, String[], substruct_dic)
     if return_visited
         return A, visited
     else
@@ -9,10 +10,12 @@ function assembly_index(t; return_visited = false) # aka "Main"
 end
 
 # Function calculating the assembly index of a compound `s`
-function _assembly_index(s, visited) # aka "AssemblyIndex"
+function _assembly_index(s, visited, dictionary) # aka "AssemblyIndex"
     if s in visited
         # no construction cost for previously visited compounds
-        return 0, visited 
+        return 0, visited
+    elseif haskey(dictionary, s)
+        return dictionary[s], [visited; s] 
     elseif length(s) <= 3
         # strings of length <= 3 can only be constructed in a set amount of combinations
         return length(s) - 1, [visited; s]
@@ -24,13 +27,15 @@ function _assembly_index(s, visited) # aka "AssemblyIndex"
         visiteds = Vector{Vector{String}}(undef, length(substructure_pairs))
 
         for (sp_idx, sp) in enumerate(substructure_pairs)
-            A1, visited1 = _assembly_index(sp[1], visited)
-            A2, visited2 = _assembly_index(sp[2], [visited1; sp[1]])
+            A1, visited1 = _assembly_index(sp[1], visited, dictionary)
+            A2, visited2 = _assembly_index(sp[2], [visited1; sp[1]], dictionary)
             As[sp_idx] = A1 + A2 + 1
             visiteds[sp_idx] = [visited2; sp[2]]
         end
 
         min_idx = argmin(As)
+        dictionary[s] = As[min_idx]
+        # println(dictionary, "\n")
         return As[min_idx], visiteds[min_idx]
     end
 end
@@ -45,11 +50,11 @@ assembly_index("ABRACADABRA", return_visited = true)
 # # tests
 assembly_index("AAAA") == 2
 assembly_index("BANANA") == 4
-# assembly_index("redrumredrumredrumredrumredrumredrumredrumredrumredrumredrum") == 9 # still takes too long
+assembly_index("redrumredrumredrumredrumredrumredrumredrumredrumredrumredrum") # still takes too long
 
 # # timing
 
 @time assembly_index("AAA")
 @time assembly_index("BANANA")
 @time assembly_index("ABNNNBA")
-@time assembly_index("ABRACADABRA")
+@time assembly_index("ABRACADABRAABRACADABRA")
