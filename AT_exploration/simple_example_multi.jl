5f5274b7986b3cbe8a9a1127ffd6c1af2cba171d
jevdnlan <jeppe.vdl@gmail.com>
2024-12-13 20:42:52 +0100

    add multiple simulations file, new figures

--- AT_exploration/simple_example_multi.jl
+++ AT_exploration/simple_example_multi.jl
@@ -1,4 +1,4 @@
-using Catalyst, OrdinaryDiffEq, Plots, GraphRecipes, JumpProcesses, DataFrames, Statistics
+using Catalyst, OrdinaryDiffEq, Plots, GraphRecipes, JumpProcesses, DataFrames, Statistics, Base.Threads
 #functions--------------------------------------------------------------------------------
 
 #function to create a reaction network
@@ -81,6 +81,32 @@ end
 function getsubstructures(s)
     return [[s[1:i], s[i+1:end]] for i in 1:(length(s)-1)]
 end
+
+function assembly_vector(sol, df)
+    assembly_vector = []
+    for i in 1:length(sol.t)
+        NT = sum(sol.u[i])
+        A = 0
+        for (j, ai) in enumerate(df.ai)
+            if sol.u[i][j] != 0
+                a = exp(ai) * (sol.u[i][j]-1) / NT
+                A += a
+            end
+        end
+        append!(assembly_vector, A)
+    end
+    return assembly_vector
+end
+
+function target_vector(sol, species_strings, target)
+    target_idx = findfirst(item -> item == target, species_strings)
+    target_vector = []
+    for i in 1:length(sol.t)
+        append!(target_vector, sol.u[i][target_idx])
+    end
+    return target_vector
+end
+    
 #-----------------------------------------------------------------------------------------
 # setting up model
 
@@ -88,23 +114,21 @@ building_blocks = ["A", "B", "N"]
 upper_bound = 6
 rate = 1
 selection_rate = 10
-selection_target = "BANANA"
+selection_target = ""
 upregulated = [["B(t)", "A(t)"], ["N(t)", "A(t)"], ["NA(t)", "NA(t)"], ["BA(t)", "NANA(t)"]]
 t = default_t()
 
 #creating the reactions can take a long time
-reactions, species = create_reactions(building_blocks, upper_bound, rate; selection_target, selection_rate, upreg_reactions = upregulated)
+reactions, species = create_reactions(building_blocks, upper_bound, rate; selection_target, selection_rate, upregulated)
 @named jumpmodel = ReactionSystem(reactions, t)
 
 u0 = vcat([Symbol(replace(string(sp), "(t)" => "")) => 5000 for sp in species[1:length(building_blocks)]], [Symbol(replace(string(sp), "(t)" => "")) => 0 for sp in species[length(building_blocks)+1:end]])
-tspan = (0.0, 0.002)
+tspan = (0.0, 0.02)
 ps = []
 
 prob = DiscreteProblem(complete(jumpmodel), u0, tspan, ps)
 
 jump_prob = JumpProblem(complete(jumpmodel), prob, Direct())
-
-sol = solve(jump_prob, SSAStepper())
 
 #-----------------------------------------------------------------------------------------
 #create dataframe with names, lengths and assembly indices for plotting
@@ -113,63 +137,60 @@ sol_lengths = map(length, species_strings)
 sol_ai = map(assembly_index, species_strings)
 
 df = DataFrame(name = species_strings, length = sol_lengths, ai = sol_ai)
-#-----------------------------------------------------------------------------------------
-#Plot by length
-p = plot(title = "Sum of Variables by Length", xlabel = "Time", ylabel = "Sum", dpi = 600);
-colors = cgrad(:jet, length(unique(sol_lengths)), categorical = true);
-for (i, len) in enumerate(unique(sol_lengths))
 
-    group_indices = findall(df.length .== len)
+num_sims = 50
 
-    group_sum = [sum([sol.u[t][idx] for idx in group_indices]) for t in 1:length(sol.t)]
+# lengths = []
+# ais = []
+# assemblies = []
+# targets = []
+# times = []
+target_values = []
 
-    plot!(p, sol.t, group_sum, label = "Length $len", color = colors[i])
+p_a = plot(title = "Assembly Through Time", xlabel = "Time", ylabel = "Assembly", dpi = 600, legend=false, ylims = (0, 80));
+p_t = plot(title = "Target Species Through Time", xlabel = "Time", ylabel = "Target Species", dpi = 600, legend=false, ylims = (0, 2000));
+
+@time @threads for _ in 1:num_sims
+    
+    sol = solve(jump_prob, SSAStepper())
+    # push!(times, sol.t)
+
+    # len_data = []
+    # for (i, len) in enumerate(unique(sol_lengths))
+
+    #     group_indices = findall(df.length .== len)
+
+    #     group_sum = [sum([sol.u[t][idx] for idx in group_indices]) for t in 1:length(sol.t)]
+
+    #     push!(len_data, group_sum)
+    # end
+    # push!(lengths, len_data)
+    
+    # assembly_index_data = []
+    # for (i, ai) in enumerate(unique(sol_ai))
+
+    #     group_indices = findall(df.ai .== ai)
+
+    #     group_sum = [sum([sol.u[t][idx] for idx in group_indices]) for t in 1:length(sol.t)]
+
+    #     push!(assembly_index_data, group_sum)
+    # end
+    # push!(ais, assembly_index_data)
+    
+    av = assembly_vector(sol, df)
+    plot!(p_a, sol.t, av, alpha = 0.3, color = :blue);
+    # push!(assemblies, av)
+
+    target_of_interest = "BANANA"
+    tv = target_vector(sol, species_strings, target_of_interest)
+    append!(target_values, tv[end])
+    plot!(p_t, sol.t, tv, alpha = 0.3, color = :red);
+    # push!(targets, tv)
 end
 
-display(p)
-# savefig(p, "AT_exploration/fig/simple_example_length_selection.png")
-
-#Plot by assembly index
-p_ai = plot(title = "Sum of Variables by Assembly Index", xlabel = "Time", ylabel = "Sum", dpi = 600);
-colors = cgrad(:jet, length(unique(sol_ai)), categorical = true);
-for (i, ai) in enumerate(unique(sol_ai))
-
-    group_indices = findall(df.ai .== ai)
-
-    group_sum = [sum([sol.u[t][idx] for idx in group_indices]) for t in 1:length(sol.t)]
-
-    plot!(p_ai, sol.t, group_sum, label = "Assembly Index $ai", color = colors[i])
-end
-
-display(p_ai)
-# savefig(p_ai, "AT_exploration/fig/simple_example_ai_selection.png")
-
-#Plot assembly through time
-assembly_vec= []
-for i in 1:length(sol.t)
-    NT = sum(sol.u[i])
-    A = 0
-    for (j, ai) in enumerate(df.ai)
-        if sol.u[i][j] != 0
-            a = exp(ai) * (sol.u[i][j]-1) / NT
-            A += a
-        end
-    end
-    append!(assembly_vec, A)
-end
-
-p_a = plot(title = "Assembly Through Time", xlabel = "Time", ylabel = "Assembly", dpi = 600, legend=false);
-plot!(p_a, sol.t, assembly_vec);
 display(p_a)
-# savefig(p_a, "AT_exploration/fig/simple_example_assembly_selection.png")
-
-target_idx = [i for (i, sp) in enumerate(species_strings) if sp == "BANANA"][1]
-target_vec = []
-for i in 1:length(sol.t)
-    append!(target_vec, sol.u[i][target_idx])
-end
-
-p_t = plot(title = "Target Species Through Time", xlabel = "Time", ylabel = "Target Species", dpi = 600, legend=false);
-plot!(p_t, sol.t[1:length(sol.t)], target_vec);
+# savefig(p_a, "AT_exploration/fig/simple_example_assembly_50sims.png")
+average = mean(target_values)
+annotate!(p_t, 0.012, -200, text("Average number of target species formed: $average", 6, :red, :left));
 display(p_t)
-# savefig(p_t, "AT_exploration/fig/simple_example_target_selection.png")
+# savefig(p_t, "AT_exploration/fig/simple_example_target_50sims.png")
