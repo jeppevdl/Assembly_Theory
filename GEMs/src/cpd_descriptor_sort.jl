734f1fcc846cd24c159d270dc4e48340e0e7e0ea
jevdnlan <jeppe.vdl@gmail.com>
2025-05-10 00:48:28 +0200

    minor change

--- GEMs/src/cpd_descriptor_sort.jl
+++ GEMs/src/cpd_descriptor_sort.jl
@@ -1,6 +1,6 @@
 using Pkg
 if basename(pwd()) != "src"
-    cd("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/src")
+    cd(@__DIR__)
 end
 Pkg.activate(".")
 
@@ -22,7 +22,9 @@ function KEGGAPI.kegg_get(query::Vector{String}, option::String, retries::Int)
 	end
 end
 
-# use code below to get the pathways and compounds from KEGG in case of no prefetched molfiles ---------------------
+# use code below to get the pathways and compounds from KEGG in case of no prefetched molfiles 
+#SKIP to next code block in other case--------------------------------------------------------------------------------------------------------
+
 pathway = "pathway"
 
 # Get unique paths, unique compounds and relationship between both
@@ -59,13 +61,13 @@ end
 @showprogress for (cpd, mol) in allmol
     if mol !== missing
 		id = split(cpd, ":")[2]
-        if !isfile("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/data/molfiles/$id.mol")
-			open("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/data/molfiles/$id.mol", "w") do f
+        if !isfile("../data/molfiles/$id.mol")
+			open("../data/molfiles/$id.mol", "w") do f
 				write(f, mol)
 			end
 		end
-		if !isfile("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/bin/assembly_go/molfiles/$id.mol")
-			open("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/bin/assembly_go/molfiles/$id.mol", "w") do f
+		if !isfile("../bin/assembly_go/molfiles/$id.mol")
+			open("../bin/assembly_go/molfiles/$id.mol", "w") do f
 				write(f, mol)
 			end
 		end
@@ -75,7 +77,7 @@ end
 
 # use this code block below if mol files are already downloaded and MA values are precalculated
 
-all_ma = CSV.read("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/data/bash_MA_output/complete_MAs.csv", DataFrame)
+all_ma = CSV.read("../data/bash_MA_output/complete_MAs.csv", DataFrame)
 complexities = DataFrame(id=String[], n_atoms=Dict{Symbol,Int}[], hybr=Vector{Int}[], n_rings=Vector{Int}[], ma=Vector{Int64}[], mass = Vector{Float64}[])
 @info "Calculating complexity of compounds..."
 @showprogress for row in eachrow(all_ma)
@@ -84,7 +86,7 @@ complexities = DataFrame(id=String[], n_atoms=Dict{Symbol,Int}[], hybr=Vector{In
 	if ma != "na"
 		try
 			# println(id)
-			graph = MolecularGraph.sdftomol("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/data/molfiles/$cpd.mol")
+			graph = MolecularGraph.sdftomol("../data/molfiles/$cpd.mol")
 		
 			n_atoms = MolecularGraph.atom_counter(graph)
 			hybr = MolecularGraph.hybridization(graph)
@@ -145,62 +147,4 @@ complexities.sp3_perc = complexities.sp3 ./ (complexities.counted_atoms)
 sort!(complexities, [:ma, :counted_atoms, :sp3, :sp2, :sp, :counted_rings], rev=true)
 @info "Done sorting compounds based on complexity"
 
-# ma_values = JSON3.read("data/MA_values.json", Dict)
-# code below to read in precalculated complexities
-# json_data = JSON3.read(open("data/pathway_complexities/complexities_$pathway.json", "r"))
-# columns = json_data[:columns] 
-# complexities = DataFrame(columns, json_data[:colindex][:names])
-
-# skip this if we calculate all compounds with the same algorithm
-# # Get MA values for each compound
-# @info "Retrieving MA values for compounds..."
-# complexities.ma = Vector{Union{Missing,Float64}}(missing, nrow(complexities))
-# @showprogress for i in 1:nrow(complexities)
-# 	cpd = complexities.id[i]
-# 	if haskey(ma_values, cpd)
-# 		complexities.ma[i] = ma_values[cpd]["MA"]
-# 	end
-# end
-
-# n = count(x -> !ismissing(x), complexities.ma);
-# @info "Found MA values for $n compounds"
-
-# cd("assembly_go")
-
-# function run_with_timeout(command::Cmd, timeout::Float64)
-
-#     stdout_buffer = IOBuffer()
-#     stderr_buffer = IOBuffer()
-
-#     p = run(pipeline(command; stdout = stdout_buffer, stderr = stderr_buffer), wait = false)
-
-#     t_start = time()
-#     # check if output is returned or time limit is exceeded
-#     while isempty(readchomp(seekstart(stdout_buffer))) && (time() - t_start < timeout)
-#         sleep(0.1)
-#     end
-
-#     if isopen(p)
-#         kill(p, Base.SIGINT)
-#     end
-
-#     # HACK: It seems one has to access the data before converting it to a String, unsure why
-#     println(stdout_buffer.data)
-#     println(stderr_buffer.data)
-
-#     stdout_str = deepcopy(String(stdout_buffer.data))
-#     stderr_str = deepcopy(String(stderr_buffer.data))
-
-#     return stdout_str, stderr_str
-# end
-
-# @info "Calculating MA values using molecular assembly algorithm..."
-# @showprogress for i in nrow(complexities):-1:1
-# 	println(complexities.id[i])
-# 	cmd = `./assembly.exe molfiles/$(complexities.id[i]).mol`
-# 	stdout, stderr = run_with_timeout(cmd, 10.0)
-# 	#...
-# end
-# @info "Done calculating MA values"
-
-CSV.write("C:/Users/jeppe/OneDrive/Documenten/Bioinformatics/Tweede master/Master Thesis/Assembly_Theory/GEMs/data/pathway_complexities/complete_descriptors.csv", complexities; header=true)
+CSV.write("../data/pathway_complexities/complete_descriptors.csv", complexities; header=true)
